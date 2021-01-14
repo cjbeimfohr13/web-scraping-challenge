57295fa653cf84c5b9f1a34a96e758a809b8388a
Catherine Beimfohr <cjbeimfohr13@gmail.com>
2021-01-13 23:37:21 -0500

    updating index.html

--- Mission_To_Mars/app.py
+++ Mission_To_Mars/app.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
-# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
-import pymongo
-
-# Create an instance of our Flask app.
 app = Flask(__name__)
 
-
-# Set route
 @app.route('/')
-
+def index():
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
