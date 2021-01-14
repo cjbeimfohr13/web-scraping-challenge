1b82d53e951d70b7893d3836511daf71ede0764d
Catherine Beimfohr <cjbeimfohr13@gmail.com>
2021-01-14 00:13:43 -0500

    updating mars hemispheres

--- Mission_To_Mars/app.py
+++ Mission_To_Mars/app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    
+    return render_template("index.html", listings=listings)
 
+@app.route("/scrape")
+def scraper():
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
