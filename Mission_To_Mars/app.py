ce75960c77739c41c2b1ddf7b12d35193aaee243
Catherine Beimfohr <cjbeimfohr13@gmail.com>
2021-01-14 20:58:55 -0500

    updating python files

--- Mission_To_Mars/app.py
+++ Mission_To_Mars/app.py
@@ -3,6 +3,10 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+conn = 'mongodb://localhost:27017'
+
+client = pymongo.MongoClient(conn)
+
 @app.route('/')
 def index():
     return render_template("index.html", listings=listings)
