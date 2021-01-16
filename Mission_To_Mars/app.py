3c423d9afe4ae2400e819a9a9d948e592b738c54
Catherine Beimfohr <cjbeimfohr13@gmail.com>
2021-01-15 22:16:24 -0500

    updating scrape_mars and app.py

--- Mission_To_Mars/app.py
+++ Mission_To_Mars/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 import pymongo
+import scrape_mars
 
 app = Flask(__name__)
 
@@ -9,13 +10,20 @@ conn = 'mongodb://localhost:27017'
 
 client = pymongo.MongoClient(conn)
 
+# db = client.collection(create a collection for the database mars)
+mars = db.mars
+
 @app.route('/')
 def index():
+    
     return render_template("index.html", listings=listings)
 
 @app.route("/scrape")
 def scraper():
-    mission_to_mars.scrape()
+    listings = mongo.db.listings
+    listings_data = scrape_craigslist.scrape()
+    listings.update({}, listings_data, upsert=True)
+    return redirect("/", code=302)
     return()
 
     
--- Mission_To_Mars/scrape_mars.py
+++ Mission_To_Mars/scrape_mars.py
@@ -10,6 +10,9 @@ def browser():
     browser = Browser('chrome', **executable_path, headless=False)
 
 def scrape():
+    browser = init_browser()
+    listings = {}
+    
     url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     browser.visit(url)
     html=browser.html
@@ -76,5 +79,5 @@ def scrape():
         
     hem_images_list
 
-
+    return listings
 
